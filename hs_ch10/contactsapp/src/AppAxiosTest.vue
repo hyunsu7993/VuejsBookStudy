4333d9c4fd9db1369f7c040546e3fc71786cbf13
hyunsu7993 <jsusu7993@gmail.com>
2020-03-30 13:18:23 +0900

    commit

--- hs_ch10/contactsapp/src/AppAxiosTest.vue
+++ hs_ch10/contactsapp/src/AppAxiosTest.vue
@@ -1,95 +1,131 @@
 <template>
-<div id="app">
+  <div id="app">
     <div class="container">
-        <div class="form-group">
-            <button @click="fetchContacts">1페이지 연락처 조회</button>
-        </div>
+      <div class="form-group">
+        <button @click="fetchContacts">1페이지 연락처 조회</button>
+      </div>
 
-        <div class="form-group">
-            <input type="text" v-model="name" placeholder="이름을 입력합니다.">
-            <input type="text" v-model="tel" placeholder="전화번호를 입력합니다.">
-            <input type="text" v-model="address" placeholder="주소를 입력합니다.">
-            <button @click="addContact">연락처 1건 추가</button>
-        </div>
+      <div class="form-group">
+        <input type="text" v-model="name" placeholder="이름을 입력합니다." />
+        <input type="text" v-model="tel" placeholder="전화번호를 입력합니다." />
+        <input type="text" v-model="address" placeholder="주소를 입력합니다." />
+        <button @click="addContact">연락처 1건 추가</button>
+      </div>
 
-        <div class="form-group">
-            <input type="text" v-model="no"><button @click="fetchContactOne">연락처 1건 조회</button>
-        </div>
+      <div class="form-group">
+        <input type="text" v-model="no" />
+        <button @click="fetchContactOne">연락처 1건 조회</button>
+      </div>
 
-        <div class="form-group">
-            <input type="text" v-model="no">
-            <input type="text" v-model="name" placeholder="이름을 입력합니다.">
-            <input type="text" v-model="tel" placeholder="전화번호를 입력합니다.">
-            <input type="text" v-model="address" placeholder="주소를 입력합니다.">
-            <button @click="updateContact">수정</button>
-        </div>
+      <div class="form-group">
+        <input type="text" v-model="no" />
+        <input type="text" v-model="name" placeholder="이름을 입력합니다." />
+        <input type="text" v-model="tel" placeholder="전화번호를 입력합니다." />
+        <input type="text" v-model="address" placeholder="주소를 입력합니다." />
+        <button @click="updateContact">수정</button>
+      </div>
 
-        <div class="form-group">
-            <input type="text" v-model="no"><button @click="deleteContact">삭제</button>
-        </div>
+      <div class="form-group">
+        <input type="text" v-model="no" />
+        <button @click="deleteContact">삭제</button>
+      </div>
 
-        <div class="form-group">
-            <input type="text" v-model="no">
-            <input type="file" ref="photofile" name="photo">
-            <button @click="changePhoto">파일 변경</button>
-        </div>
+      <div class="form-group">
+        <input type="text" v-model="no" />
+        <input type="file" ref="photofile" name="photo" />
+        <button @click="changePhoto">파일 변경</button>
+      </div>
     </div>
 
     <span>JSON 출력</span>
     <div id="result" class="container">
-        <xmp>{{result}}</xmp>
+      <xmp>{{result}}</xmp>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-    name: "app",
-    data() {
-        return {
-            no: 0,
-            name: '',
-            tel: '',
-            address: '',
-            result: null
-        }
+  name: "app",
+  data() {
+    return {
+      no: 0,
+      name: "",
+      tel: "",
+      address: "",
+      result: null
+    };
+  },
+  methods: {
+    fetchContacts: function() {
+      axios
+        .get("/api/contacts", {
+          params: { pageno: 1, pagesize: 5 }
+        })
+        .then(response => {
+          console.log(response);
+          this.result = response.data;
+        })
+        .catch(ex => {
+          console.log("ERROR!!!! : ", ex);
+        });
     },
-    methods: {
-        fetchContacts: function(){
-            axios.get('/api/contacts', {
-                params:{pageno:1, pagesize:5}
-            })
-            .then((response) => {
-                console.log(response);
-                this.result = response.data;
-            })
-            .catch((ex)=>{
-                console.log("ERROR!!!! : ",ex)
-            })
-        },
-        addContact(){
-            
-        },
-        fetchContactOne(){
-            axios.get('/api/contacts/'+this.no)
-            .then((response) => {
-                console.log(response);
-                this.result=response.data;
-            })
-        },
-        updateContact(){
-            
-        },
-        deleteContact(){
-
-        },
-        changePhoto(){
-
-        }
+    addContact() {
+      axios.post("/api/contacts", {
+        name: this.name,
+        tel: this.tel,
+        address: this.address
+      })
+      .then((response)=>{
+          console.log(response);
+          this.result = response.daa;
+          this.no = response.data.no;
+      })
+      .catch((ex)=>{
+          console.log("ERROR!!!! : ", ex);
+      })
     },
-}
+    fetchContactOne() {
+      axios.get("/api/contacts/" + this.no).then(response => {
+        console.log(response);
+        this.result = response.data;
+      });
+    },
+    updateContact() {
+        axios.put('/api/contacts/'+this.no,{
+            name: this.name,
+            tel: this.tel,
+            address: this.address
+        })
+        .then((response)=>{
+            console.log(response);
+            this.name='';
+            this.tell='';
+            this.address='';
+            this.result=response.data;
+        })
+        .catch((ex)=>{
+            console.log("ERROR!!!! : ", ex)
+        })
+    },
+    deleteContact() {
+        axios.delete('/api/contacts/'+this.no)
+        .then((response) => {
+            console.log(response);
+            this.no=0;
+            this.result=response.data;
+        })
+        .catch((ex)=>{
+            console.log("ERROR!!! : ", ex)
+        })
+    },
+    changePhoto() {
+        
+    }
+  }
+};
 </script>
 
 <style>
@@ -104,19 +140,19 @@ export default {
   margin-top: 60px;
 }
 
-.container{
-    border: 1px solid red;
-    padding:10px;
-    margin-bottom: 10px;
-    text-align:left;
+.container {
+  border: 1px solid red;
+  padding: 10px;
+  margin-bottom: 10px;
+  text-align: left;
 }
 #result {
-    text-align: left;
-    padding:20px;
-    border: 1px solid black;
+  text-align: left;
+  padding: 20px;
+  border: 1px solid black;
 }
 .form-group {
-    border: 1px dashed gray;
-    padding: 5px 5px 5px 20px;
+  border: 1px dashed gray;
+  padding: 5px 5px 5px 20px;
 }
 </style>AppAxiosTest.vue
